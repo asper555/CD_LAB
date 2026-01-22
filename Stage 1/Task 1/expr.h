861fd4032fe8f3012ae76171cdf368f3861e10d0
Poosa Yashwanth <144501783+asper555@users.noreply.github.com>
2026-01-22 23:17:15 +0530

    Stage 1 Task 1 done

---
+++ Stage 1/Task 1/expr.h
@@ -0,0 +1,22 @@
+#ifndef EXPTREE_H
+#define EXPTREE_H
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+typedef struct tnode{
+    int val;
+    char* op;
+    struct tnode* left;
+    struct tnode* right;
+}tnode;
+
+tnode* makeLeafNode(int n);
+tnode* makeOperatorNode(char op, tnode *l, tnode *r);
+
+void prefix(tnode *t);
+void postfix(tnode *t);
+
+#endif
